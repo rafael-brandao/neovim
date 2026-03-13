65469c8248a9ed9c739e7b1fcc348fd5daf53d84
Rafael Brandão <rafa.bra.dev@pm.me>
2026-03-13 03:16:20 -0300

    feat: add custom filetype associations

--- packages/nvf/config/extra-lua.nix
+++ packages/nvf/config/extra-lua.nix
@@ -2,17 +2,36 @@
   vim = {
     additionalRuntimePaths = [./extra-lua];
 
-    luaConfigRC.customGlobalsScript =
-      nvf.lib.nvim.dag.entryBetween ["basic"] ["globalsScript"]
-      # lua
-      ''
-        local M = {
-          custom = {
-            f = require("custom.f"),
-          },
-          fn   = require("custom.f").fn,
-          data = assert(vim.fn.stdpath("data")),
-        }
-      '';
+    luaConfigRC = {
+      customGlobalsScript =
+        nvf.lib.nvim.dag.entryBetween ["basic"] ["globalsScript"]
+        # lua
+        ''
+          local M = {
+            custom = {
+              f = require("custom.f"),
+            },
+            fn   = require("custom.f").fn,
+            data = assert(vim.fn.stdpath("data")),
+          }
+        '';
+
+      filetype-mappings =
+        nvf.lib.nvim.dag.entryBefore ["lazyConfigs"]
+        # lua
+        ''
+          -- Custom filetype associations
+          vim.filetype.add({
+            extension = {
+              -- h = "c",   -- uncomment if you ever need it
+            },
+            filename = {
+              ["flake.lock"] = "json",
+              [".yamlfmt"]   = "yaml",
+              [".yamllint"]  = "yaml",
+            },
+          })
+        '';
+    };
   };
 }

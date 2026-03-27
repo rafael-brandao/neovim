73e44e293906c8bd6c411650af1ecea735c7557e
Rafael Brandão <rafa.bra.dev@pm.me>
2026-03-27 05:38:27 -0300

    feat: configure initial git support

---
+++ packages/nvf/config/plugins/git.nix
@@ -0,0 +1,16 @@
+_: {
+  vim.git = {
+    gitsigns = {
+      enable = true;
+      setupOpts = {
+        # signs = {
+        #   add.text = "+";
+        #   change.text = "~";
+        #   changedelete.text = "~";
+        #   delete.text = "_";
+        #   topdelete.text = "‾";
+        # };
+      };
+    };
+  };
+}

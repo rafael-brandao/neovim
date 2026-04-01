2c1a0ed9c1c7ac6549230ce8b3ec4ffa7ed5ef04
Rafael Brandão <rafa.bra.dev@pm.me>
2026-04-01 15:42:45 -0300

    feat: extract nvf modules for external consumption
    Bundle _module.args.nvf injection into the module list so consumers
    don't need to handle it manually. Expose default and dev variants via
    flake.modules.nvf and simplify perSystem packages to consume them
    directly.

--- packages/nvf/flake-part.nix
+++ packages/nvf/flake-part.nix
@@ -1,28 +1,68 @@
 {inputs, ...}: {
-  # flake = {
-  #   modules.rb.nvf = ./config;
-  # };
+  flake = {
+    modules = {
+      rb.nvf = rec {
+        default = [
+          {
+            _module.args = {inherit (inputs) nvf;};
+          }
+          ./config
+        ];
+        dev = default ++ [./dev];
+      };
+    };
+
+    overlays = {
+      rb-nvf = final: prev: let
+        inherit (final.stdenv.hostPlatform) system;
+      in {
+        neovim-nightly = inputs.neovim-nightly.packages.${system}.neovim;
+
+        vimPlugins = with final.vimUtils;
+          prev.vimPlugins
+          // {
+            blink-cmp-nightly = inputs.blink-cmp.packages.${system}.blink-cmp;
+
+            modes-nvim-git = buildVimPlugin {
+              name = "modes.nvim";
+              src = inputs.modes-nvim;
+              doCheck = false;
+            };
+            snacks-nvim-git = buildVimPlugin {
+              name = "snacks.nvim";
+              src = inputs.snacks-nvim;
+              doCheck = false;
+            };
+            tiny-inline-diagnostic-nvim-git = buildVimPlugin {
+              name = "tiny-inline-diagnostic.nvim";
+              src = inputs.tiny-inline-diagnostic-nvim;
+              doCheck = false;
+            };
+            todo-comments-nvim-git = buildVimPlugin {
+              name = "todo-comments.nvim";
+              src = inputs.todo-comments-nvim;
+              doCheck = false;
+            };
+            trouble-nvim-git = buildVimPlugin {
+              name = "trouble.nvim";
+              src = inputs.trouble-nvim;
+              doCheck = false;
+            };
+          };
+      };
+    };
+  };
   perSystem = {pkgs, ...}: let
     mkNvfPkg = modules:
       (inputs.nvf.lib.neovimConfiguration {
-        inherit pkgs;
-        modules =
-          modules
-          ++ [
-            {
-              _module.args = {inherit (inputs) nvf;};
-            }
-          ];
+        inherit modules pkgs;
       }).neovim;
   in {
-    packages = {
-      nvf = mkNvfPkg [
-        ./config
-      ];
-      nvf-dev = mkNvfPkg [
-        ./config
-        ./dev
-      ];
+    legacyPackages = {
+      rb.nvf = with inputs.self.modules; {
+        default = mkNvfPkg rb.nvf.default;
+        dev = mkNvfPkg rb.nvf.dev;
+      };
     };
   };
 }

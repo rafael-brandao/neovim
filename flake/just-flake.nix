9a64d90a7ade5472cd4bc153afdd2be7ed57e9cb
Rafael Brandão <rafa.bra.dev@pm.me>
2026-05-30 10:16:35 -0300

    feat(just): add variadic arguments support to nvf recipes
    Updates the run, inspect, and shorthand recipes to accept variadic 
    arguments. This allows passing Nix flags (like --show-trace) directly 
    through just commands.

--- flake/just-flake.nix
+++ flake/just-flake.nix
@@ -12,22 +12,22 @@
           ''
             # Run a neovim package: just run nvf | just run nvf-dev
             [arg("pkg", pattern="^(default|nvf|nvf-dev)$")]
-            run pkg=("default"):
-              nix run .#{{pkg}}
-
+            run pkg=("default") *args:
+              nix run .#{{pkg}} {{args}}
+            
             # Inspect a neovim package: just inspect nvf | just inspect nvf-dev
             [arg("pkg", pattern="^(default|nvf|nvf-dev)$")]
-            inspect pkg=("default"):
-              nix build .#{{pkg}}
+            inspect pkg=("default") *args:
+              nix build .#{{pkg}} {{args}}
               ./result/bin/nvf-print-config | bat --language lua
-
+            
             [group: 'shorthands']
-            nvf:
-              just run nvf
-
+            nvf *args:
+              just run nvf {{args}}
+            
             [group: 'shorthands']
-            nvf-dev:
-              just run nvf-dev
+            nvf-dev *args:
+              just run nvf-dev {{args}}
           '';
       };
     };
